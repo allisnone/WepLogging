2482933a7d699d312698d131e3f482c5dd6a0385
allisnone <zgx20022002@163.com>
2019-02-20 23:57:23 +0800

    update department

--- self_define_org.py
+++ self_define_org.py
@@ -1,75 +1,132 @@
 # -*- coding: utf-8 -*-
-def read_file(file='custom_user0.csv',encoding='utf-8'):
-    txtfile = open(file, 'r', encoding='utf-8')
+def read_file(file='custom_user0.csv',encoding='utf-8',type='r'):
+    """
+    :param file: str
+    :param encoding: str
+    :param type: str
+    :return: list, 按行读取文件的数据列表，每一行最后字符为\n
+    """
+    txtfile = open(file, type, encoding=encoding)
     lines = txtfile.readlines()
     return lines
 
-def get_standard_template_file(csv='custom_user0.csv'):
+def get_standard_template_colums(file='custom_user0.csv',encoding='utf-8'):
     #['\ufeffuuid', 'commonName', 'mail', 'logonName', 'ip', 'enableManager', 'title', 'manager', 'telephone', 'department', 'departmentName\n']
-    txtfile = open(csv, 'r', encoding='utf-8')
-    users = txtfile.readlines()
-    columns = users[0].split(',')
-    return  columns
-
-def check_file(from_file='custom_user.csv', standard_columns=[],department_uuids=[]):
     """
-    用于url分类测试，测试文件中存放大量的url地址
-    :param from_file: str 
-    :return: list， users（Generator）
+    :param file: str
+    :param encoding: str
+    :return: list, 返回文件第一行，文件头
     """
-    txtfile = open(from_file, 'r',encoding='utf-8')
-    users = txtfile.readlines()
-    columns = users[0].split(',')
+    datas = read_file(file, encoding)
+    if datas:
+        return datas[0].replace('\n','').split(',')
+    else:
+        return []
+    
+def check_csv_head(csv_file,standard_csv_file='custom_user0.csv',standard_columns=[]):
+    """
+    天空卫士自定义组织机构，检查csv 文件头的有效性
+    :param csv_file: str, department/custom_user/computer csv file
+    :param standard_columns: list, standard custom_user columns
+    :return: list, 有错误的用户数据
+    """
+    if not standard_columns:
+        standard_columns = get_standard_template_colums(file=standard_csv_file)
+    datas = read_file(csv_file)
+    if not datas:
+        print('ERROR: 读取的文件%s无数据' % csv_file)
+        return []
+    else:
+        if len(datas)==1:
+            print('ERROR: 读取的文件%s仅有文件头' % csv_file)
+            return []
+        else:
+            pass
+    columns = datas[0].replace('\n','').split(',')
     different_columns = list(set(standard_columns).difference(set(columns)))
     if different_columns:
-        print('%s 比标准模板少了以下几列：%s'% (from_file, different_columns))
+        print('ERROR-缺少文件头字段： %s 比标准模板少几列：%s'% (csv_file, different_columns))
     different_columns1 = list(set(columns).difference(set(standard_columns)))
     if different_columns1:
-        print('%s 比标准模板多了以下几列：%s' % (from_file, different_columns1))
-    columns_count = len(columns)
+        print('ERROR-多余文件头字段：%s 比标准模板多几列：%s' % (csv_file, different_columns1))
+    if not different_columns and not different_columns1:
+        return datas,standard_columns,True
+    else:
+        return datas,standard_columns,False
+
+def check_custom_user(datas, standard_columns,uniq_index=[0,1,2,3,9],department_uuids=[]):
+    """
+    天空卫士自定义组织机构，检查custom_user.csv 文件的有效性
+    :param csv_file: str, custom_user csv file
+    :param standard_columns: list, standard custom_user columns
+    :param department_uuids: list, valid department uuid list
+    :return: list, 有错误的用户数据
+    """
+    columns_count = len(standard_columns)
     error_lines = []
-    V23_NON_NUM_INDEX_USER = [0,1,2,3,9]
-    uniq_count = len(V23_NON_NUM_INDEX_USER)
+    #uniq_index = [0,1,2,3,9]
+    uniq_count = len(uniq_index)
     uniq_list = [[]]*uniq_count
-    for i in range(1,len(users)):
-        user_list = users[i].split(',')
-        #检查是否确实字段
-        
+    if not department_uuids:
+        uniq_list = [['1']]*uniq_count
+    for i in range(1,len(datas)):
+        raw_user_datas = datas[i]
+        user_list = raw_user_datas.replace('\n','').split(',')
+        #检查每个用户的字段数是否足够
         if len(user_list)==columns_count:
             pass
         else:
-            print('ERROR user i=%s user_list: %s' % (i, user_list[i]))
+            print('ERROR-字段数不匹配： 第%s行，用户数据: %s' % (i, raw_user_datas))
             error_lines.append(user_list)
-        #检查非空字段
-        k = 0
+        #检查非空字段是否为空和唯一性字段是否唯一
+        if i>5:
+            pass
+            #break
         for k in range(0,uniq_count):
-            j = V23_NON_NUM_INDEX_USER[k]
+            j = uniq_index[k]
             value = user_list[j]
+            print('i=%s k=%s j= %s value=%s'%(i,k,j,value))
+            print('uniq_list[%s]=%s' % (k,uniq_list[k]))
             if value:
-                if value in uniq_list[k] and standard_columns[j]!='department':
-                    print('%s 字段中 %s 重复' %(standard_columns[j], value))
-                else:
-                    uniq_list[k].append(value)
-                pass
+                print('department_uuids=',department_uuids)
+                if department_uuids:#用户检查
+                    if value in uniq_list[k] and standard_columns[j]!='department':
+                        print('ERROR-custom_user唯一性字段不唯一，第%s行 ，重复字段 %s=%s， 用户数据: %s' %(i+1, standard_columns[j], value, raw_user_datas))
+                    else:
+                        uniq_list[k].append(value)
+                else:#部门检查
+                    if value in uniq_list[k]:
+                        print('ERROR-department唯一性字段不唯一，第%s行 ，重复字段 %s=%s， 用户数据: %s' %(i+1, standard_columns[j], value, raw_user_datas))
+                    else:
+                        uniq_list[k].append(value)
             else:
-                print('ERROR 非空字段检查出错，行号i=%s 第%s个字段%s为空，该行内容%s' % (i+1, j , stand_c[j], user_list))
+                print('ERROR-非空字段为空，第%s行，第%s字段%s为 空字符，用户数据：%s' % (i+1, j+1 , standard_columns[j], raw_user_datas))
                 error_lines.append(user_list)
+            #检查uuid是否重复
             if j==0: #uuid
-                if value in department_uuids:
-                    print('user_uuid 和 department_uuid重复')
+                if department_uuids and value in department_uuids:
+                    print('ERROR-部门和用户uuid重复混淆，第%s行 ，uuid=%s， 用户数据: %s' %(i+1, value, raw_user_datas))
                 else:
                     pass
-            elif j==9: #部门uuid是否存在
-                if value in department_uuids:
+            elif j==9: #部门uuid是否存在,且有效
+                if department_uuids and value in department_uuids:
                     pass
                 else:
-                   print('用户的 department_uuid不存在') 
+                   print('ERROR-用户部门不存在，第%s行 ，用户部门：%s， 用户数据: %s' %(i+1, value, raw_user_datas))
             else:
                 pass
             
-    return error_lines
+    return uniq_list,error_lines
 
-def get_department_uuid():
+def get_department_uuid(csv_file, standard_columns):
+    """
+    天空卫士自定义组织机构，检查department.csv 文件的有效性
+    :param csv_file: str, custom_user csv file
+    :param standard_columns: list, standard custom_user columns
+    :param department_uuids: list, valid department uuid list
+    :return: list, 有错误的用户数据
+    """
+    datas,is_healthy_header = check_csv_head(csv_file, standard_columns)
     department_datas = read_file(file='department.csv')
     department_uuids = ['1']
     for i in range(1,len(department_datas)):
@@ -81,7 +138,7 @@ def get_department_uuid():
             if parent_department_id in department_uuids:
                 pass
             else:
-                print('ERROR 上级部门必须先定义: 第%s行部门的parent_department_id=%s未定义，部门信息: %s' % (i+1, parent_department_id,department_datas[i]))
+                print('ERROR-上级部门必须先定义: 第%s行部门的parent_department_id=%s未定义，部门信息: %s' % (i+1, parent_department_id,department_datas[i]))
             #uuid 重复性检查
             if uuid not in department_uuids:
                 department_uuids.append(uuid)
@@ -89,11 +146,16 @@ def get_department_uuid():
                 print('第%s行部门的uuid重复，重复的uuid: %s, 部门信息: %s' % (i+1, uuid,department_datas[i]))
     return department_uuids
 
-V23_NON_NUM_INDEX_USER = [0,1,2,3,9]
-stand_c = get_standard_template_file(csv='custom_user0.csv')
-print('stand_c=',stand_c)
-from_file='custom_user.csv'
 
+custom_user_csv = 'custom_user.csv'
+standard_custom_user_csv ='custom_user0.csv'
+department_csv = 'department.csv'
+standard_department_csv = 'department0.csv'
+department_datas,department_standard_columns,is_healthy_department_headers = check_csv_head(department_csv,standard_csv_file=standard_department_csv)
+print('department_datas=',department_datas)
+department_uniq_list,error_department_lines = check_custom_user(department_datas, department_standard_columns,uniq_index=[0,1],department_uuids=[])
+department_uuids = department_uniq_list[0]
+print('department_uuids=',department_uniq_list)
+user_datas,user_standard_columns,is_healthy_user_headers = check_csv_head(custom_user_csv,standard_csv_file=standard_custom_user_csv)
 
-department_uuids = get_department_uuid()
-check_file(from_file,stand_c,department_uuids)
+#user_uniq_list,error_user_lines = check_custom_user(user_datas, user_standard_columns,uniq_index=[0,1,2,3,9],department_uuids=department_uuids)
